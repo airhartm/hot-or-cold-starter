cb7747ec17547ca3a5815effa9ab03751248183c
Michael Airhart <mike@airhart.me>
2015-12-08 20:14:16 -0500

    First commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+//initialize variables
+//new game
+//rand
+//guess input and validation 1-100
+//feedback
+//guess list and count
+
+
+//initialize variables
+var guessRand
+var guessEntry=0
+var guessArray[]
+var guessCount=0
+var guessTemp 
 
 $(document).ready(function(){
 	
@@ -11,6 +25,35 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+	//get dom objects
+	$selectNew = $('a.new');
+	$form = $('form');
+	$counter = $('#count');
+	$guesses = $('#guessList');
+
+	function newgame () {
+		guessRand=parseInt(math.Random()*100)
+		guessTemp = "Make your guess!"
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
 
